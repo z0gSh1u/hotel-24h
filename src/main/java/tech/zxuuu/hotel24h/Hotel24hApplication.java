74bc9931db81eb03b615edffd0e0cde61f98768e
z0gSh1u <zx.cs@qq.com>
2020-09-04 10:18:20 +0800

    first commit.

---
+++ src/main/java/tech/zxuuu/hotel24h/Hotel24hApplication.java
@@ -0,0 +1,13 @@
+package tech.zxuuu.hotel24h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hotel24hApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Hotel24hApplication.class, args);
+  }
+
+}
---
+++ src/test/java/tech/zxuuu/hotel24h/Hotel24hApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.zxuuu.hotel24h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hotel24hApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
